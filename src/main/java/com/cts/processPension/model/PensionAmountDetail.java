e851426b439ae2e37335dc37506dcd0dc33a6c14
SGhosh-007 <gsusho123@gmail.com>
2022-05-30 00:46:30 +0530

    first commit

---
+++ src/main/java/com/cts/processPension/model/PensionAmountDetail.java
@@ -0,0 +1,72 @@
+package com.cts.processPension.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Data to be stored in the database
+ * 
+ *  @author Sushobhan Ghosh
+ *  @author 890054
+ *
+ */
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+@Entity
+public class PensionAmountDetail {
+
+	
+
+	@Id
+	private String aadhaarNumber;
+
+	@Column
+	private Double pensionAmount;
+
+	@Column
+	private Double bankServiceCharge;
+
+	@Column
+	private Double finalAmount;
+
+	public String getAadhaarNumber() {
+		return aadhaarNumber;
+	}
+
+	public void setAadhaarNumber(String aadhaarNumber) {
+		this.aadhaarNumber = aadhaarNumber;
+	}
+
+	public Double getPensionAmount() {
+		return pensionAmount;
+	}
+
+	public void setPensionAmount(Double pensionAmount) {
+		this.pensionAmount = pensionAmount;
+	}
+
+	public Double getBankServiceCharge() {
+		return bankServiceCharge;
+	}
+
+	public void setBankServiceCharge(Double bankServiceCharge) {
+		this.bankServiceCharge = bankServiceCharge;
+	}
+
+	public Double getFinalAmount() {
+		return finalAmount;
+	}
+
+	public void setFinalAmount(Double finalAmount) {
+		this.finalAmount = finalAmount;
+	}
+	
+}
